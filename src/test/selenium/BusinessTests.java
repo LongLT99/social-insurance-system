42710965305b8995df61522d9648099e85b217bc
nhkhoi22 <52534768+nhkhoi22@users.noreply.github.com>
2021-05-25 01:31:16 +0700

    Add test nav link of business home

---
+++ src/test/selenium/BusinessTests.java
@@ -0,0 +1,53 @@
+package test.selenium;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BusinessTests extends SeleniumTestDriver {
+	
+	ChromeDriver driver = getDriver();
+	
+	public void login() {
+		driver.get("http://localhost:8080/social-insurance-system/login.jsp");
+		WebElement username = driver.findElement(By.name("username"));
+		WebElement password = driver.findElement(By.name("password"));
+		WebElement loginBtn = driver.findElement(By.name("btnlogin"));
+		username.sendKeys("tnhhA");
+		password.sendKeys("123456");
+		loginBtn.click();
+	}
+	
+	@Test
+	public void testNotLogin() {
+		driver.get("http://localhost:8080/social-insurance-system/business/businessHome.jsp");
+		String targetUrl = driver.getCurrentUrl();
+		String expectedUrl = "http://localhost:8080/social-insurance-system/login.jsp?err=timeout";
+		assertEquals(expectedUrl, targetUrl);
+	}
+	
+	@Test
+	public void testBussinessHomeTitle() {
+		login();
+		String targetUrl = driver.getCurrentUrl();
+		String title = driver.getTitle();
+		String expectedTitle = "Hệ thống hỗ trợ tính phí bảo hiểm xã hội";
+		assertEquals(expectedTitle, title);
+	}
+	
+	@Test
+	public void testBussinessHomeNavLink() {
+		login();
+		assertTrue(driver.findElement(By.xpath("/html/body/nav/div/ul/li[1]/a")).isEnabled());
+	}
+	
+	@After
+	public void closeDriver() {
+		driver.close();
+	}
+}
