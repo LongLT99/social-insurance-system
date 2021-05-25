af160c32782bbe3910f2190fc4bd4fb1f6c516d6
nhkhoi22 <52534768+nhkhoi22@users.noreply.github.com>
2021-05-26 01:39:31 +0700

    Add valid test for signup

--- src/test/selenium/BusinessTests.java
+++ src/test/selenium/BusinessTests.java
@@ -3,16 +3,26 @@ package test.selenium;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.sql.SQLException;
+
 import org.junit.After;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.mysql.jdbc.Connection;
+
+import src.dao.LabourDAO;
+
 public class BusinessTests extends SeleniumTestDriver {
-	
+
 	ChromeDriver driver = getDriver();
 	
+	LabourDAO dao = new LabourDAO();
+	
+	Connection conn = (Connection) dao.conn;
+
 	public void login() {
 		driver.get("http://localhost:8080/social-insurance-system/login.jsp");
 		WebElement username = driver.findElement(By.name("username"));
@@ -22,7 +32,7 @@ public class BusinessTests extends SeleniumTestDriver {
 		password.sendKeys("123456");
 		loginBtn.click();
 	}
-	
+
 	@Test
 	public void testNotLogin() {
 		driver.get("http://localhost:8080/social-insurance-system/business/businessHome.jsp");
@@ -30,7 +40,7 @@ public class BusinessTests extends SeleniumTestDriver {
 		String expectedUrl = "http://localhost:8080/social-insurance-system/login.jsp?err=timeout";
 		assertEquals(expectedUrl, targetUrl);
 	}
-	
+
 	@Test
 	public void testBussinessHomeTitle() {
 		login();
@@ -39,29 +49,88 @@ public class BusinessTests extends SeleniumTestDriver {
 		String expectedTitle = "Hệ thống hỗ trợ tính phí bảo hiểm xã hội";
 		assertEquals(expectedTitle, title);
 	}
-	
+
 	@Test
 	public void testBussinessHomeNavLink() {
 		login();
 		WebElement stayHome = driver.findElement(By.xpath("/html/body/nav/div/ul/li[1]/a"));
 		assertTrue(stayHome.isEnabled());
-		
+
 	}
-	
+
 	@Test
 	public void testInfoNavLink() {
 		login();
 		WebElement goToLabourInfo = driver.findElement(By.xpath("/html/body/nav/div/ul/li[2]/a"));
 		assertTrue(goToLabourInfo.isEnabled());
 	}
-	
+
 	@Test
 	public void testViewLink() {
 		login();
 		WebElement goToInsuranceInfo = driver.findElement(By.xpath("/html/body/nav/div/ul/li[3]/a"));
 		assertTrue(goToInsuranceInfo.isEnabled());
 	}
-	
+
+	// Test khong nhap ten
+	@Test
+	public void addInvalidLabour() {
+		login();
+		driver.get("http://localhost:8080/social-insurance-system/business/addLabour.jsp");
+		WebElement name = driver.findElement(By.name("name"));
+		WebElement idNumber = driver.findElement(By.name("idNumber"));
+		WebElement insCode = driver.findElement(By.name("insCode"));
+		WebElement dob = driver.findElement(By.name("dob"));
+		WebElement gender = driver.findElement(By.name("gender"));
+		WebElement nationality = driver.findElement(By.name("nationality"));
+		WebElement ethnic = driver.findElement(By.name("ethnic"));
+		WebElement isCD = driver.findElement(By.name("isCD"));
+		WebElement btnAdd = driver.findElement(By.name("btnAdd"));
+		idNumber.sendKeys("023901428908");
+		insCode.sendKeys("24902319450");
+		dob.sendKeys("1997-10-23");
+		gender.sendKeys("1");
+		nationality.sendKeys("Viet Nam");
+		ethnic.sendKeys("Kinh");
+		isCD.sendKeys("true");
+		btnAdd.click();
+		String targetUrl = driver.getCurrentUrl();
+		String expectedUrl = "http://localhost:8080/social-insurance-system/business/addLabour.jsp";
+		assertEquals(expectedUrl, targetUrl);
+	}
+
+	// Test nhap hop le
+	@Test
+	public void addValidLabour() {
+		login();
+		driver.get("http://localhost:8080/social-insurance-system/business/addLabour.jsp");
+		WebElement name = driver.findElement(By.name("name"));
+		WebElement idNumber = driver.findElement(By.name("idNumber"));
+		WebElement insCode = driver.findElement(By.name("insCode"));
+		WebElement dob = driver.findElement(By.name("dob"));
+		WebElement gender = driver.findElement(By.name("gender"));
+		WebElement nationality = driver.findElement(By.name("nationality"));
+		WebElement ethnic = driver.findElement(By.name("ethnic"));
+		WebElement isCD = driver.findElement(By.name("isCD"));
+		WebElement btnAdd = driver.findElement(By.name("btnAdd"));
+		name.sendKeys("Nguyen Hoang Khoi");
+		idNumber.sendKeys("023901428908");
+		insCode.sendKeys("24902319450");
+		dob.sendKeys("1997-10-23");
+		gender.sendKeys("1");
+		nationality.sendKeys("Viet Nam");
+		ethnic.sendKeys("Kinh");
+		isCD.sendKeys("true");
+		btnAdd.click();
+		
+		try {
+			conn.setAutoCommit(false);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	@After
 	public void closeDriver() {
 		driver.close();
--- src/test/selenium/SignUpTests.java
+++ src/test/selenium/SignUpTests.java
@@ -11,8 +11,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
@@ -42,7 +44,7 @@ public class SignUpTests extends SeleniumTestDriver {
 	@Test
 	public void signupTitleTest() {
 		String title = driver.getTitle();
-		String expectedTitle = "Đăng ký BHXH";
+		String expectedTitle = "ﾄ斉ハg kﾃｽ BHXH";
 		assertEquals(expectedTitle, title);
 	}
 
@@ -541,51 +543,65 @@ public class SignUpTests extends SeleniumTestDriver {
 
 	// Test dang ky hop le
 	@Test
-	public void signUpValiddTest() {
+	public void signUpValiddTest() throws InterruptedException {
 		WebElement username = driver.findElement(By.name("username"));
 		WebElement password = driver.findElement(By.name("password"));
 		WebElement madonvi = driver.findElement(By.name("madonvi"));
 		WebElement ten = driver.findElement(By.name("ten"));
 		WebElement masothue = driver.findElement(By.name("masothue"));
-		WebElement tencqbh = driver.findElement(By.name("tencqbh"));
+		Select tencqbh = new Select(driver.findElement(By.name("tencqbh")));
 		WebElement sdt = driver.findElement(By.name("sdt"));
 		WebElement email = driver.findElement(By.name("email"));
-		WebElement tinh = driver.findElement(By.name("tinh"));
-		WebElement quan = driver.findElement(By.name("quan"));
-		WebElement phuong = driver.findElement(By.name("phuong"));
-		WebElement chitiet = driver.findElement(By.name("chitiet"));
-		WebElement signupBtn = driver.findElement(By.name("btnSignup"));
+		Select tinh = new Select(driver.findElement(By.name("tinh")));
+		Select quan = new Select(driver.findElement(By.name("quan")));
+		Select phuong = new Select(driver.findElement(By.name("phuong")));
+		JavascriptExecutor js = (JavascriptExecutor) driver;
 		username.sendKeys("nhkhoi");
-		password.sendKeys("123456");
+		password.sendKeys("12345678");
 		madonvi.sendKeys("1hjdajhdf");
 		ten.sendKeys("tnhhC");
 		masothue.sendKeys("038954923");
-		tencqbh.sendKeys("1");
+		tencqbh.selectByVisibleText("Cơ quan BHXH Quận Ba Đình");
 		sdt.sendKeys("0924823948");
 		email.sendKeys("tnhhB@gmail.com");
-		tinh.sendKeys("1");
-		quan.sendKeys("1");
-		phuong.sendKeys("2");
-		chitiet.sendKeys("1");
-		signupBtn.click();
+		tinh.selectByVisibleText("Thành phố Hà Nội");
+		WebElement quand = driver
+				.findElementByXPath("/html/body/section/div[1]/div/div/form/div[9]/div/select[2]/option[6]");
+		js.executeScript("$('#tinh').trigger('change')");
+		quand.click();
+		WebElement phuongd = driver
+				.findElementByXPath("/html/body/section/div[1]/div/div/form/div[9]/div/select[3]/option[2]");
+		phuongd.click();
+		WebElement chitiet = driver.findElementByXPath("/html/body/section/div[1]/div/div/form/div[9]/div/input[3]");
+		WebElement signupBtn = driver.findElementByXPath("/html/body/section/div[1]/div/div/form/div[10]/input");
+		chitiet.sendKeys("abc");
+		signupBtn.submit();
 		String targetUrl = driver.getCurrentUrl();
-		String expectedUrl = "http://localhost:8080/social-insurance-system/register.jsp";
+		String expectedUrl = "http://localhost:8080/social-insurance-system/login.jsp?idok=1";
 		assertEquals(expectedUrl, targetUrl);
 
 		try {
-			String sql = "SELECT * FROM thanhvien WHERE " + "username = 'nhkhoi' AND password = 'tnhhB2134'";
+			String sql = "SELECT * FROM thanhvien WHERE " + "username = 'nhkhoi' AND password = '12345678'";
 			PreparedStatement ps = (PreparedStatement) conn.prepareStatement(sql);
 			ResultSet rs = ps.executeQuery();
 			assertTrue(rs.next());
-			sql = "DELETE FROM DONVIBH WHERE " + "MADONVI = '1hjdajhdf'";
-			ps = (PreparedStatement) conn.prepareStatement(sql);
-			ps.executeUpdate();
-			sql = "DELETE FROM thanhvien WHERE " + "username = 'nhkhoi' AND password = 'tnhhB2134'";
-			ps = (PreparedStatement) conn.prepareStatement(sql);
-			ps.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			PreparedStatement ps;
+			String sql = "DELETE FROM DONVIBH WHERE " + "MADONVI = '1hjdajhdf'";
+			try {
+				ps = (PreparedStatement) conn.prepareStatement(sql);
+				ps.executeUpdate();
+				sql = "DELETE FROM thanhvien WHERE " + "username = 'nhkhoi' AND password = '12345678'";
+				ps = (PreparedStatement) conn.prepareStatement(sql);
+				ps.executeUpdate();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
 	}
 
